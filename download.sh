66853d56f2cbb9eadebd7f650987d78ca3028d55
Atsuhiro Hada <atsuhiro.hada@ist.osaka-u.ac.jp>
2022-06-30 01:57:35 +0900

    update

--- download.sh
+++ download.sh
@@ -4,3 +4,4 @@ filename=train2014.zip
 html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
 curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o ${filename}
 unzip train2014.zip
+rm train2014.zip
